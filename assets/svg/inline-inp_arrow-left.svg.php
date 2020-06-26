75db61d2cc563969bbf7b39fa0e37649c47c37db
svsdesign <simon@svsdesign.co.uk>
2020-06-26 11:58:20 +0100

    commit_088

--- assets/svg/inline-inp_arrow-left.svg.php
+++
@@ -1,8 +0,0 @@
-<?php 
-/*
- ICON : Arrow Left
-*/
-?>
-<svg class="svg-icon arrow-left-icon" width="27px" height="40px" viewBox="0 0 27 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-<polygon points="25.715,0 26.288,0.736 1.522,20 26.288,39.262 25.715,40 0,20 "/>
-</svg>
